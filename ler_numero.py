e0069eb436e99fc360d18a21b160ab5593a1d4b4
gleicimarribeiropereiraa-hue <gleicimarribeiropereiraa@gmail.com>
2026-05-20 21:43:08 -0300

    Add files via upload
    atividade corrigida

--- ler_numero.py
+++ ler_numero.py
@@ -1,3 +1,17 @@
 '''
-ler numero inteiro n.Escrever a soma de todos os numeros pare de 2 até n.
+5 ler numero inteiro n.Escrever a soma de todos os numeros pare de 2 até n.
 '''
+
+n: int = int(input("Digite o valor de n: "))
+
+soma:int = 0
+
+contador:int = 2
+
+while contador< n:
+    
+    if contador % 2 == 0:
+       soma = soma + contador
+    
+    contador +=2
+print(f"a soma dos pares até {n}  é igual a {soma}")
