2c15ddde15e71b8d5a550a2c93851be1fc216abe
gleicimarribeiropereiraa-hue <gleicimarribeiropereiraa@gmail.com>
2026-06-15 21:11:24 -0300

    Add files via upload
    exemplo

---
+++ exemplo.py
@@ -0,0 +1,11 @@
+'''Uma oficina mecanica precisa  de um sistema  para administrar os serviços
+é importante  saber  sobre os carros marca,modelo,ano  e sobre os proprietários nome,telefone cpf,também é importante saber o tipo de serviço,data e o valor em que o serviço foi realizado
+
+    Carro ->Marca ->Modelo -> Ano N---1   proprietario->Nome->telefone ->cpf
+        N
+        |
+        N
+Serviço-> tipo de serviço ->data ->valor
+
+
+'''
