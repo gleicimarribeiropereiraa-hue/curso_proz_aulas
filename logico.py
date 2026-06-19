174ffb51178f361d189823e41e0124ca28f69b2e
gleicimarribeiropereiraa-hue <gleicimarribeiropereiraa@gmail.com>
2026-06-19 18:34:36 -0300

    Add files via upload
    atualizados

--- logico.py
+++ logico.py
@@ -1,43 +1,44 @@
 '''
-CREATE TABLE(Hóspede
+CREATE TABLE Hospede(
 id:int pk  NN
 nome:varchar(255) NN
 documento: varchar(255) NN Unique
 data_de_nascimento:date
 telefone:varchar(255))
 _ _ _ _ _ _ _ _ _ _ __ _ _ _ _ _
-CREATE TABLE(Quarto
+CREATE TABLE Quarto(
 id:int pk
 tipo:varchar(100)
 status: bool
 numero: varchar(100))
 _ _ _ _ _ _ _ _ _ _ _
 
-CREATE TABLE (Reserva
+CREATE TABLE Reserva(
 id:int pk
-data_reseva: date
+data_reserva: date
 hospede_id:int FK
 quarto_id:int fk
-numero:varchar(100)
 
 '''
 
 
 '''
-CREATE TABLE (Colaborador
+
+RH
+CREATE TABLE Colaborador (
 id: int pk
 nome:varchar(200)
 matricula:varchar(255)
 setor: varchar(255)
 data_nascimento : date  (idade)
-id_cargo)
+id_cargo:int fk )
 __ _ _ _ _ _ _ _ _ _ 
 
-CREATE TABLE (Cargo
+CREATE TABLE Cargo (
 id:int pk
+nome: varchar(100)
 codigo:varchar(255)
 descrição: text
-nome: varchar(100)
 valor:Float)
 
 
