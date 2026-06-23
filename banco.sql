e38df6eb604d090bdd04375f6a9102f607729e29
gleicimarribeiropereiraa-hue <gleicimarribeiropereiraa@gmail.com>
2026-06-23 19:48:48 -0300

    Update banco.sql
    atualizado as tabelas criadas

--- banco.sql
+++ banco.sql
@@ -1,16 +1,26 @@
 Create Table if NOT EXISTS cliente(
-    ID INTEGER  PRIMARY KEY AUTOINCREMENT, 
-    NOME VARCHAR(255) NOT NULL,
-    TELEFONE VARCHAR(20),
-  	EMAIL VARCHAR(50)
+    id INTEGER  PRIMARY KEY AUTOINCREMENT, 
+    nome VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20),
+  	email VARCHAR(50)
 );
 
 Create Table if NOT EXISTS carros(
-    ID INTEGER   PRIMARY KEY AUTOINCREMENT, 
+    id INTEGER   PRIMARY KEY AUTOINCREMENT, 
     modelo VARCHAR(20),
     placa VARCHAR(255) NOT NULL UNIQUE,
   	ano VARCHAR(255) NOT NULL,
   	marca VARCHAR(50)
 );
 
-INSERT Into  carros(MODELO, PLACA, ANO, MARCA) VALUES('AUDI Q3','4875JH','2025','AUDI')
+Create Table if NOT EXISTS servicos(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  tipo_servico VARCHAR(255),
+  custo FLOAT,
+  servico_car INTEGER,
+  FOREIGN KEY (servico_car) REFERENCES carros(ID));
+  
+  
+INSERT INTO cliente(nome,telefone, email) VALUES ('Paulo','3121242142','p@gmail.com');
+INSERT Into  carros(MODELO, PLACA, ANO, MARCA) VALUES('AUDI Q3','4875JH','2025','AUDI');
+INSERT INTO  servicos(tipo_servico,custo,servico_car) VALUES('Refeito Motor','5000',1)
