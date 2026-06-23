63946d1243d5d58f9195f9de2600df21ff41ec0a
gleicimarribeiropereiraa-hue <gleicimarribeiropereiraa@gmail.com>
2026-06-23 20:15:30 -0300

    Update banco.sql
    comandos de
    atualização de campos e exclusão

--- banco.sql
+++ banco.sql
@@ -1,10 +1,11 @@
+/*CRIAÇÃO DA TABELA CLIENTE*/
 Create Table if NOT EXISTS cliente(
     id INTEGER  PRIMARY KEY AUTOINCREMENT, 
     nome VARCHAR(255) NOT NULL,
     telefone VARCHAR(20),
   	email VARCHAR(50)
 );
-
+/*cRIAÇÃO DA TABELA CARROS*/ 
 Create Table if NOT EXISTS carros(
     id INTEGER   PRIMARY KEY AUTOINCREMENT, 
     modelo VARCHAR(20),
@@ -12,14 +13,19 @@ Create Table if NOT EXISTS carros(
   	ano VARCHAR(255) NOT NULL,
   	marca VARCHAR(50)
 );
-
+/*INSERIR CAMPO*/
+ALTER TABLE carros ADD COLUMN cor varchar(80);
+/*EXCLUIR CAMPO*/
+ALTER TABLE carros DROP COLUMN cor varchar(80);
+/*CRIAÇÃO DA TABELA SERVIÇOS*/
 Create Table if NOT EXISTS servicos(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   tipo_servico VARCHAR(255),
   custo FLOAT,
   servico_car INTEGER,
   FOREIGN KEY (servico_car) REFERENCES carros(ID));
-  
+  /*renomear coluna*/
+  ALTER TABLE cliente RENAME COLUMN telefone TO contato
   
 INSERT INTO cliente(nome,telefone, email) VALUES ('Paulo','3121242142','p@gmail.com');
 INSERT Into  carros(MODELO, PLACA, ANO, MARCA) VALUES('AUDI Q3','4875JH','2025','AUDI');
